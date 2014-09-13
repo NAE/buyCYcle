2928b04a54545b5ed2f75094ff7803c45cbaa8b9
CreationEdge <creationedge@gmail.com>
2014-09-13 13:28:12 -0500

    .

--- php/rentslot.php
+++ php/rentslot.php
@@ -1,6 +1,6 @@
 <?php
 
-		echo "<br />Please take your bike from stall #".$currentSlot[0].".";
+		
 		$update = $db->prepare("UPDATE `Slots` SET `unlock`=1 WHERE `rackid`='".$currentRack."' AND `slotnum`='".$currentSlot[0]."';");
 		$update->execute();
 		$update = null;
