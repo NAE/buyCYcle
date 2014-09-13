52c5cd2c7844f1844e9b83e07735e4497936279d
CreationEdge <creationedge@gmail.com>
2014-09-13 13:07:30 -0500

    .

--- php/rent.php
+++ php/rent.php
@@ -12,7 +12,7 @@
 		$update->execute();
 		$update = null;
 		
-		$ready == "no";
+		$ready = "no";
 		while ($ready == "no") {
 			if(!$isTaken) {
 				$takenQRY = $db->prepare("SELECT slotnum FROM Slots WHERE slotnum='".$currentSlot[0]."' AND hasbike=0 LIMIT 1;");
