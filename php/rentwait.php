beea3d69b767289c6683c330aa59944bd32df571
CreationEdge <creationedge@gmail.com>
2014-09-13 14:22:47 -0500

    .

--- php/rentwait.php
+++ php/rentwait.php
@@ -2,22 +2,13 @@
 		$currentSlot = $_POST['currentSlot'];
 		$currentRack = $_POST['currentRack'];
 		
-		$ready == "no";
-		while ($ready == "no") {
 
-			if(!$isTaken) {
-				$takenQRY = $db->prepare("SELECT slotnum FROM Slots WHERE rackid='".$currentRack."' AND slotnum='".$currentSlot."' AND hasbike=0 LIMIT 1;");
-				$takenQRY->execute();
-				
-				$isTaken = $takenQRY->fetch(PDO::FETCH_BOTH);
-				
-				$takenQRY = null;
-			}
-			else {
-				$ready = "yes";
-			}
-		}
+		$takenQRY = $db->prepare("SELECT slotnum FROM Slots WHERE rackid='".$currentRack."' AND slotnum='".$currentSlot."' AND hasbike=0 LIMIT 1;");
+		$takenQRY->execute();
 		
-		echo "Thank you";
+		$isTaken = $takenQRY->fetch(PDO::FETCH_BOTH);
 		
+		$takenQRY = null;
+
+		echo $isTaken;
 ?>
