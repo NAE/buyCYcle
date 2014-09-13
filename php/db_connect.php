e6bdda9c9068e5d84f0b9ed3c1650604310d1f32
CreationEdge <creationedge@gmail.com>
2014-09-13 09:54:01 -0500

    db_connect
    PDO format

--- php/db_connect.php
+++ php/db_connect.php
@@ -4,6 +4,20 @@
 Open of DB connection
 */
 
-$db=mysql_connect("localhost", "root", "ageoldjim") or die(mysql_error());
+//$db=mysql_connect("localhost", "root", "ageoldjim") or die(mysql_error());
+
+$dsn = 'mysql:host=localhost;dbname=BuyCycle';
+$un = 'root';
+$pw = 'ageoldjim';
+
+
+
+
+try {
+    $db = new pdo($dsn,$un,$pw);
+}catch (PDOException $e) {
+    echo "Failed to connect to the database: (" . $e->getMessage() . "\n";
+}
+
 
 ?>
