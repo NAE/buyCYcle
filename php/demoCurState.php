346ace4fec306cbb6c9eadfa6d676373f4bac5b3
PaintThePoet <mplee92@iastate.edu>
2014-09-13 16:04:27 -0500

    More

--- php/demoCurState.php
+++ php/demoCurState.php
@@ -9,26 +9,7 @@
 </head>
 
 <body style="background-color: #004125;">
-		<table>
-			<tr>
-				<?php
-				for($i = 0; $i < 12; $i++) {
-					echo "<td>".$i."</td>";
-				}
-				?>
-			</tr>
-		</table>
-		<?php
-			$qry = $db->prepare("SELECT * FROM BuyCycle.Slots WHERE rackid = '12B' ORDER BY slotnum ASC");
-			$qry->execute();
-			$all = $qry->fetchAll(PDO::FETCH_BOTH);
-			$qry = null;
-
-			foreach($all as $pkey) {
-				$cur = "";
-
-			}
-		?>
+	
 </body>
 
 </html>
