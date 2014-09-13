64db79d9de77ccc88302d6082ba2202591600fb4
NAE <nileproject@hotmail.com>
2014-09-13 12:55:48 -0500

    test

--- php/map/getracks.php
+++ php/map/getracks.php
@@ -1,11 +1,22 @@
 <?php
 require_once('../db_connect.php');
 
+//sql to get all the racks
 $sql = "SELECT * FROM Racks ORDER BY rackid";
+
+//sql to get all the slots with bikes for this rack
+$sql2 = "SELECT * FROM Slots WHERE rackid=" . $rackid . " AND hasbike=1";
 
 $sth = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 $sth->execute();
 $racks = $sth->fetchAll();
+
+foreach($rack in $racks){
+	$sth2 = $db->prepare($sql2, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+	$sth2->execute();
+	$slots = $sth2->fetchAll();
+	$rack['odata'] = count($slots);
+}
 
 echo json_encode($racks);
 
