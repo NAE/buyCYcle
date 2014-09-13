4fb6541ca037bcdd1653a14a14a34b71f0c089f7
NAE <nileproject@hotmail.com>
2014-09-13 11:04:01 -0500

    test

--- pushhook.php
+++ pushhook.php
@@ -5,6 +5,6 @@ ini_set('display_startup_errors',1);
 error_reporting(-1);
 
 echo chdir("/var/www/Bike");
-echo shell_exec("sudo cd /var/www/Bike; sudo git pull");
+echo shell_exec("sudo cd /var/www/Bike && sudo git pull");
 
 ?>
