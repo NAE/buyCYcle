ab0bfb0d7829b9dc1ee1c47b3e98e881e615b4ed
CreationEdge <creationedge@gmail.com>
2014-09-13 13:17:44 -0500

    .

--- php/rent.php
+++ php/rent.php
@@ -18,7 +18,7 @@
 				$takenQRY = $db->prepare("SELECT slotnum FROM Slots WHERE slotnum='".$currentSlot[0]."' AND hasbike=0 LIMIT 1;");
 				$takenQRY->execute();
 				
-				$isTaken = takenQRY->fetch(PDO::FETCH_BOTH);
+				$isTaken = $takenQRY->fetch(PDO::FETCH_BOTH);
 				
 				$takenQRY = null;
 			}
