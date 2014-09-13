464e436ffea52e483ef99a78b24b9007ae6b2f45
NAE <nileproject@hotmail.com>
2014-09-13 10:55:16 -0500

    test

--- pushhook.php
+++ pushhook.php
@@ -4,7 +4,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-echo chdir("sudo /var/www/Bike");
+echo chdir("/var/www/Bike");
 echo shell_exec("git pull");
 
 ?>
