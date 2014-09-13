6e5fbbb95874e3c3143121ec4be0c27089539e0a
NAE <nileproject@hotmail.com>
2014-09-13 10:04:02 -0500

    testing db connect

--- php/map.php
+++ php/map.php
@@ -1,8 +1,13 @@
 <?php
 
-include 'connection.php';
+include 'db_connect.php';
 
+$sql = "SELECT * FROM Racks";
 
+$sth = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$sth->execute();
+$testOutput = $sth->fetchAll();
+print_r($testOutput);
 
 ?>
 
