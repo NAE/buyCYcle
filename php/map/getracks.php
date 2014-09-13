49ecb4fd604b8c96ecbaaf6b90b50d8aca3e1f1e
NAE <nileproject@hotmail.com>
2014-09-13 13:56:10 -0500

    test

--- php/map/getracks.php
+++ php/map/getracks.php
@@ -13,7 +13,7 @@ $racks = $sth->fetchAll();
 $i = 0;
 
 foreach($racks as $rack){
-	$sql2 = "SELECT COUNT(*) FROM Slots WHERE rackid=" . $rack['rackid'] . " AND hasbike=1";
+	$sql2 = "SELECT COUNT(*) FROM Slots WHERE rackid=\'" . $rack['rackid'] . "\' AND hasbike=1";
 	$sth2 = $db->prepare($sql2);
 	$sth2->execute();
 	$slots = $sth2->fetchAll(PDO::FETCH_BOTH);
