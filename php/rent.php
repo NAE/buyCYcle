607891ddd5a618ac68bfbb015d9380d95ca48261
CreationEdge <creationedge@gmail.com>
2014-09-13 11:41:08 -0500

    .

--- php/rent.php
+++ php/rent.php
@@ -1,7 +1,7 @@
 <?php
 	require_once('php/db_connect.php');
 	
-	$qry = $db->prepare("SELECT slot FROM slots WHERE rackid=".$currentRack." AND hasbike=1 LIMIT 1;");
+	$qry = $db->prepare("SELECT slot FROM slots WHERE rackid='".$currentRack."' AND hasbike=1 LIMIT 1;");
 	$qry->execute();
 	$currentSlot = $qry->fetch(PDO::FETCH_BOTH);
 	$qry = null;
