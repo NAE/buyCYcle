96b6b755d680ecc44f6fd6b00d166af32c304595
PaintThePoet <mplee92@iastate.edu>
2014-09-13 16:00:11 -0500

    More curState

--- php/demoCurState.php
+++ php/demoCurState.php
@@ -1,4 +1,6 @@
 <?php
+	//$db is the var name of the connected database
+	include("db_connect");
 ?>
 <html>
 
@@ -7,7 +9,26 @@
 </head>
 
 <body style="background-color: #004125;">
-	
+		<table>
+			<tr>
+				<?php
+				for($i = 0; $i < 12; $i++) {
+					echo "<td>".$i."</td>";
+				}
+				?>
+			</tr>
+		</table>
+		<?php
+			$qry = $db->prepare("SELECT * FROM BuyCycle.Slots WHERE rackid = '12B' ORDER BY slotnum ASC");
+			$qry->execute();
+			$all = $qry->fetchAll(PDO::FETCH_BOTH);
+			$qry = null;
+
+			foreach($all as $pkey) {
+				$cur = ;
+
+			}
+		?>
 </body>
 
 </html>
