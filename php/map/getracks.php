9a9c726e84b2a9037b18dd7e83c2a7213f1aba7d
NAE <nileproject@hotmail.com>
2014-09-13 13:22:53 -0500

    t

--- php/map/getracks.php
+++ php/map/getracks.php
@@ -5,13 +5,13 @@ require_once('../db_connect.php');
 $sql = "SELECT * FROM Racks ORDER BY rackid";
 
 //sql to get all the slots with bikes for this rack
-$sql2 = "SELECT * FROM Slots WHERE rackid=" . $rackid . " AND hasbike=1";
 
 $sth = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 $sth->execute();
 $racks = $sth->fetchAll();
 
 foreach($racks as $rack){
+	$sql2 = "SELECT * FROM Slots WHERE rackid=" . $rack['rackid'] . " AND hasbike=1";
 	$sth2 = $db->prepare($sql2, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 	$sth2->execute();
 	$slots = $sth2->fetchAll();
