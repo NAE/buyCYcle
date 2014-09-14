53f993e6886cb13d50ae9ac374e6e4900e3bd229
NAE <nileproject@hotmail.com>
2014-09-13 23:29:41 -0500

    h

--- php/relockSlot.php
+++ php/relockSlot.php
@@ -4,6 +4,15 @@
 	$rack = $_POST['rack'];
 	$slot = $_POST['slot'];
 	
+	//make sure that the bike can be taken
+	$qry = $db->prepare("SELECT * FROM Slots WHERE slotnum='" . $slot . "', rackid='" . $rack . "', hasbike=1, unlocked=1");
+	$qry->execute();
+	$all = $qry->fetchAll(PDO::FETCH_BOTH);
+	
+	if(count($all) < 1){
+		//there were no rows with that slot and rack that were empty
+		return;
+	}
 	
 	$qry = $db->prepare("UPDATE Slots SET `hasbike`=0 WHERE rackid='" . $rack . "' AND slotnum='" . $slot . "';");
 	$qry->execute();
