b67c44138a181b23355891c7d7b0f2a1ccccaad1
PaintThePoet <mplee92@iastate.edu>
2014-09-14 00:20:04 -0500

    more

---
+++ php/qrChoices.php
@@ -0,0 +1,18 @@
+<table>
+	<tr>
+		<td>
+			<div class="qrText">12A</div>
+		</td>
+		<td>
+			<img src="../Img/12A.jpg">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<div class="qrText">12B</div>
+		</td>
+		<td>
+			<img src="../Img/12B.jpg">
+		</td>
+	</tr>
+</table>
