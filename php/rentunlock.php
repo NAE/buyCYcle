75423fe198a08c137094ff4141890707376af4ad
CreationEdge <creationedge@gmail.com>
2014-09-13 21:30:41 -0500

    .

--- php/rentunlock.php
+++ php/rentunlock.php
@@ -1,8 +1,13 @@
 <?php
 	require_once('./db_connect.php');
-			
+		//Unlock the rack
 		$update = $db->prepare("UPDATE `Slots` SET `unlock`=1 WHERE `rackid`='".$currentRack."' AND `slotnum`='".$currentSlot[0]."';");
 		$update->execute();
 		$update = null;
-
+		
+		//Update user's timestamp
+		$update = $db->prepare("UPDATE `Users` SET `lastrented`=NOW() WHERE `userid`='846881035';");
+		$update->execute();
+		$update = null;
+		
 ?>
