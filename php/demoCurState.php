239e4a9b94a86a9932b2b2fa39f3cbab3d342484
NAE <nileproject@hotmail.com>
2014-09-13 21:35:03 -0500

    t

--- php/demoCurState.php
+++ php/demoCurState.php
@@ -17,32 +17,21 @@
 		<div style='background-image: url("../Graphics/greenLines.png"); width: 1300px; height: 340px; padding-top: 20px; radius: 5px; background-repeat: no-repeat;'>
 		<center>
 		<table>
-			<tr id="bikerow">
+			<tr id="numrow">
 				<?php
 				for($i = 1; $i <= 10; $i++) {
 					echo "<td><center><div class='numbers' style='font-size: 24px;'>".$i."</div></center></td>";
 				}
 				?>
 			</tr>
-			<tr id="lockrow">
-			</tr>
 				<?php
 					$qry = $db->prepare("SELECT * FROM Slots WHERE rackid='12B'");
 					$qry->execute();
 					$all = $qry->fetchAll(PDO::FETCH_BOTH);
 					$qry = null;
-			echo "<tr>";
-					foreach($all as $pkey) {
-						$cur = $pkey['hasbike'];
-						if($cur == 1) {
-							echo "<td><img class='imgSize' src='../Graphics/greenBike.png'></td>";
-						}
-						else {
-							echo "<td><img class='imgSize' src='../Graphics/redBike.png'></td>";
-						}
-					}
+			echo "<tr id='bikerow'>";
 			echo "</tr>
-			<tr>";
+			<tr id='lockopen'>";
 					foreach($all as $pkey) {
 						$cur = $pkey['unlock'];
 						if($cur == 1) {
@@ -64,7 +53,23 @@
 
 function updateContents(){
 	$.post("getCurState.php", {rack: "12B"}, function(data){
+		data = $.parseJSON(data);
 		console.log(data);
+		for (var bike in data){
+			var slotNum = bike['slotnum'];
+			var rackId = bike['rackid'];
+			var hasBike = bike['hasbike'];
+			var unlock = bike['unlock'];
+			
+			var tdString;
+			if(hasBike){
+				tdString = "<td><img class='imgSize' src='../Graphics/greenBike.png'></td>";
+			}else{
+				tdString = "<td><img class='imgSize' src='../Graphics/redBike.png'></td>";
+			}
+			
+			$(tdString).appendTo("#bikerow");
+		}
 	});
 	setTimeout("updateContents();", 1000);
 }
