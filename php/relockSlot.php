d0ecfec3fd4faed4d60f61c22560e7b72e6f7620
NAE <nileproject@hotmail.com>
2014-09-13 22:48:30 -0500

    t

--- php/relockSlot.php
+++ php/relockSlot.php
@@ -1,6 +1,9 @@
 <?php
+	require_once('db_connect.php');
+
 	$rack = $_POST['rack'];
 	$slot = $_POST['slot'];
+	
 	
 	$qry = $db->prepare("UPDATE Slots SET hasbike=0, unlock=0 WHERE rackid='" . $rack . "' AND slotnum='" . $slot . "';");
 	$qry->execute();
