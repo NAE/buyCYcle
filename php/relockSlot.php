bd841067b957749d9f2e81d30e1ba8cf710138f0
NAE <nileproject@hotmail.com>
2014-09-13 22:43:38 -0500

    t

--- php/relockSlot.php
+++ php/relockSlot.php
@@ -2,7 +2,7 @@
 	$rack = $_POST['rack'];
 	$slot = $_POST['slot'];
 	
-	$sql = "UPDATE Slots SET hasbike=0, unlock=0 WHERE rackid='" . $rack . "' AND slotnum='" . $slot . "';";
+	$qry = $db->prepare("UPDATE Slots SET hasbike=0, unlock=0 WHERE rackid='" . $rack . "' AND slotnum='" . $slot . "';");
 	$qry->execute();
 	$all = $qry->fetchAll(PDO::FETCH_BOTH);
 
